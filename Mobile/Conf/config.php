1a86800eb2dce939e4046ca0b3b3fcaaafc21872
mz121star <mz121star@gmail.com>
2014-08-06 15:13:42 +0800

    page

---
+++ Mobile/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+$common_config = include APP_PATH.'../Conf/config.php'; //公共配置
+$private_config = array(
+                           'URL_ROUTER_ON' => true,
+                           'URL_CASE_INSENSITIVE' =>true,
+                           'URL_ROUTE_RULES' => array(
+                                                    'shop/detail/:shopid' => 'Shop/index',
+                                                ));//私有配置
+return array_merge($common_config, $private_config);
