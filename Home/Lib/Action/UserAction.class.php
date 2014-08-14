29b51114c1c4afcc100302d477b495068be958e5
mz121star <mz121star@gmail.com>
2014-08-14 11:42:54 +0800

    aaa

---
+++ Home/Lib/Action/UserAction.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class ShopAction extends PublicAction {
+
+  public function index(){
+    $this->display();
+  }
+    public function changeinfo(){
+      $this->display();
+    }
+      public function myfav(){
+        $this->display();
+      }
+        public function orderdetail(){
+          $this->display();
+        }
+}
