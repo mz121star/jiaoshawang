e05a39bbbda312f6b8b099a39596caba4e187a3d
zhaobin <zhaobin@deepin.deepin-domain>
2014-08-22 17:25:18 +0800

    add

--- Conf/config.php
+++ Conf/config.php
@@ -3,6 +3,6 @@ return array(
     'DB_PREFIX' => 'dc_',
     'DB_DSN' => 'mysql://root:820819@localhost:3306/jiaosha',
 
-    'SHOW_PAGE_TRACE' => true,
+    'SHOW_PAGE_TRACE' => false,
     'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes'
 );
