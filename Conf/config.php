96d27693cb2045cd7525f6ad27f81c9d05acd86f
mz121star <mz121star@gmail.com>
2014-08-22 17:52:31 +0800

    aa

--- Conf/config.php
+++ Conf/config.php
@@ -3,7 +3,7 @@ return array(
     'DB_PREFIX' => 'dc_',
     'DB_DSN' => 'mysql://root:8ecba89b81@localhost:3306/jiaosha',
       '
-    'SHOW_PAGE_TRACE' => true,
+
     'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes',
       'APP_GROUP_LIST'     => 'Home,Mobile,Admin',
              'DEFAULT_GROUP'      =>'Mobile',
