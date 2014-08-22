9a7455e07028dcb34bd1e0c7c2dc94c82943b768
mz121star <mz121star@gmail.com>
2014-08-22 17:48:16 +0800

    aa

--- Conf/config.php
+++ Conf/config.php
@@ -2,8 +2,7 @@
 return array(
     'DB_PREFIX' => 'dc_',
     'DB_DSN' => 'mysql://root:8ecba89b81@localhost:3306/jiaosha',
-                                                                 'APP_GROUP_LIST'     => 'Home,Mobile,Admin',
-                                                                  'DEFAULT_GROUP'      =>'Home',
+
     'SHOW_PAGE_TRACE' => true,
     'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes',
 
@@ -12,7 +11,7 @@ return array(
         *格式如: '子域名'=>array('分组名/[模块名]','var1=a&var2=b');
         */
         'APP_SUB_DOMAIN_RULES'=>array(
-            'm'=>array('Mobile/[Index]'),  // admin域名指向Admin分组
+            'm'=>array('Mobile/'),  // admin域名指向Admin分组
 
         ),
 );
