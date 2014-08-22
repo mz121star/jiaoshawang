c1cfc5c16d0f12e31afd971a3aa03f42f3e1c269
mz121star <mz121star@gmail.com>
2014-08-22 17:51:07 +0800

    aa

--- Conf/config.php
+++ Conf/config.php
@@ -2,10 +2,11 @@
 return array(
     'DB_PREFIX' => 'dc_',
     'DB_DSN' => 'mysql://root:8ecba89b81@localhost:3306/jiaosha',
-
+      '
     'SHOW_PAGE_TRACE' => true,
     'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes',
-
+      APP_GROUP_LIST'     => 'Home,Mobile,Admin',
+             'DEFAULT_GROUP'      =>'Mobile',
      'APP_SUB_DOMAIN_DEPLOY'=>1, // 开启子域名配置
         /*子域名配置
         *格式如: '子域名'=>array('分组名/[模块名]','var1=a&var2=b');
