5b38eed30cf5c7958bca14096f25a7fe7e8ef353
zhaobin <zhaobin@deepin.deepin-domain>
2014-08-22 17:13:55 +0800

    add

--- Public/mobile.php
+++ Public/mobile.php
@@ -2,5 +2,5 @@
 header("content-type:text/html;charset=utf-8");
 define('APP_NAME', 'Mobile');
 define('APP_PATH', '../Mobile/');
-define('APP_DEBUG', true);
+define('APP_DEBUG', false);
 require '../ThinkPHP/ThinkPHP.php';
