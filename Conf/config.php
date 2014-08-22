411bff27c255827f66034be31253a3e9d3e49ec9
mz121star <mz121star@gmail.com>
2014-08-22 17:59:57 +0800

    aa

--- Conf/config.php
+++ Conf/config.php
@@ -5,14 +5,5 @@ return array(
 
 
     'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes',
-      'APP_GROUP_LIST'     => 'Home,Mobile,Admin',
-             'DEFAULT_GROUP'      =>'Mobile',
-     'APP_SUB_DOMAIN_DEPLOY'=>1, // 开启子域名配置
-        /*子域名配置
-        *格式如: '子域名'=>array('分组名/[模块名]','var1=a&var2=b');
-        */
-        'APP_SUB_DOMAIN_RULES'=>array(
-            'm'=>array('Mobile/'),  // admin域名指向Admin分组
 
-        ),
 );
