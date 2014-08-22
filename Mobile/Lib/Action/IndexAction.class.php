2deaa44fb6d4ec45578d5dbbfd66d167be52a503
zhaobin <zhaobin@deepin.deepin-domain>
2014-08-22 14:22:41 +0800

    add

--- Mobile/Lib/Action/IndexAction.class.php
+++ Mobile/Lib/Action/IndexAction.class.php
@@ -3,6 +3,32 @@
 class IndexAction extends PublicAction {
 
     public function index(){
+        $userid = $this->userInfo['user_id'];
+        $shopobj = M("Shop");
+        $orderobj = M("order");
+        import('ORG.Util.Page');
+        $count = $shopobj->count();
+        $page = new Page($count, 10);
+        $shoplist = $shopobj->field('dc_shop.id, shop_name, shop_beginworktime, shop_endworktime, shop_deliver_money, shop_deliver_beginmoney, shop_deliver_time, shop_image, shop_top, user_id,user_people')->join(' dc_peoplefav ON dc_peoplefav.user_shop = dc_shop.user_id')->order(array('dc_shop.id'=>'desc'))->limit($page->firstRow.','.$page->listRows)->select();
+        $commonshop = array();
+        $current_time = date('Gis');
+        foreach ($shoplist as $shop) {
+            $beginworktime = intval(implode('', explode(':', $shop['shop_beginworktime'])));
+            $endworktime = intval(implode('', explode(':', $shop['shop_endworktime'])));
+            if ($current_time >= $beginworktime && $current_time <= $endworktime) {
+                $shop['is_working'] = 1;
+            } else {
+                $shop['is_working'] = 0;
+            }
+            if ($userid && $shop['user_people'] == $userid) {
+                $shop['is_fav'] = 1;
+            } else {
+                $shop['is_fav'] = 0;
+            }
+            $shop['order_number'] = $orderobj->where('food_shop = "'.$shop['user_id'].'"')->count();
+            $commonshop[] = $shop;
+        }
+        $this->assign('commonshop', $commonshop);
         $this->display();
     }
        public function detail(){
--- Mobile/Lib/Action/ShopAction.class.php
+++ Mobile/Lib/Action/ShopAction.class.php
@@ -3,6 +3,37 @@
 class ShopAction extends PublicAction {
 
     public function index(){
+        $shopid = $this->_get('shopid');
+        //获取店铺菜品
+        $food = M("Food");
+        $topfoodlist = $food->where('user_id="'.$shopid.'" and food_top="1"')->order(array('id'=>'desc'))->select();
+        $this->assign('topfoodlist', $topfoodlist);
+        $commonfoodlist = $food->where('user_id="'.$shopid.'" and food_top="0"')->order(array('id'=>'desc'))->select();
+        $this->assign('commonfoodlist', $commonfoodlist);
+        //获取店铺信息
+        $shop = M("Shop");
+        $shopinfo = $shop->where('user_id="'.$shopid.'"')->find();
+        $foodnumber = count($topfoodlist) + count($commonfoodlist);
+        $this->assign('shopinfo', $shopinfo);
+        $this->assign('foodnumber', $foodnumber);
+        //获取店铺公告
+        $shopnotice = M("shopnotice");
+        $noticelist = $shopnotice->where('user_id="'.$shopid.'"')->field('id, notice_title')->order(array('notice_date'=>'desc'))->select();
+        $this->assign('noticelist', $noticelist);
+
+        $cartlist = array();
+        $carttotle = 0;
+        $cartprice = 0;
+        if (isset($_SESSION['cart']) && count($_SESSION['cart'])) {
+            $cartlist = $_SESSION['cart'];
+            $carttotle = count($_SESSION['cart']);
+            foreach ($_SESSION['cart'] as $cart) {
+                $cartprice += $cart['price']*$cart['num'];
+            }
+        }
+        $this->assign('cartlist', $cartlist);
+        $this->assign('cartprice', $cartprice);
+        $this->assign('carttotle', $carttotle);
         $this->display();
     }
 }
