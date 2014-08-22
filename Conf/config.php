2af3755d32c5280e3f2d6a9e45a03223b219fef2
mz121star <mz121star@gmail.com>
2014-08-22 17:36:20 +0800

    aa

--- Conf/config.php
+++ Conf/config.php
@@ -4,5 +4,14 @@ return array(
     'DB_DSN' => 'mysql://root:820819@localhost:3306/jiaosha',
 
     'SHOW_PAGE_TRACE' => true,
-    'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes'
+    'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes',
+
+     'APP_SUB_DOMAIN_DEPLOY'=>1, // 开启子域名配置
+        /*子域名配置
+        *格式如: '子域名'=>array('分组名/[模块名]','var1=a&var2=b');
+        */
+        'APP_SUB_DOMAIN_RULES'=>array(
+            'mobile'=>array('Mobile/'),  // admin域名指向Admin分组
+
+        ),
 );
