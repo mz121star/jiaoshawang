9d5ba13ce62e9687e7ce94d7e94137544c5be9f7
zhaobin <zhaobin@deepin.deepin-domain>
2014-12-26 10:53:20 +0800

    add

--- Api/Lib/Action/ShopAction.class.php
+++ Api/Lib/Action/ShopAction.class.php
@@ -8,7 +8,7 @@ class ShopAction extends Action {
     
     public function getshoplist_get() {
         $shop = M("Shop");
-        $shoplist = $shop->join(' dc_user ON dc_user.user_id = dc_shop.user_id')->field('dc_user.user_id,shop_name,shop_email,shop_phone,user_status')->order(array('dc_user.id'=>'desc'))->select();
+        $shoplist = $shop->join(' dc_user ON dc_user.user_id = dc_shop.user_id')->field('dc_user.user_id,shop_name,shop_image,shop_email,shop_phone,user_status')->order(array('dc_user.id'=>'desc'))->select();
         $this->response($shoplist, 'json');
     }
 }
