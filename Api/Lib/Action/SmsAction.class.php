10e5cd1a0a3af41eb56cdead78d9d8a55eaddacc
zhaobin <zhaobin@deepin.deepin-domain>
2015-03-27 17:18:40 +0800

    add

--- Api/Lib/Action/SmsAction.class.php
+++ Api/Lib/Action/SmsAction.class.php
@@ -8,22 +8,31 @@ class SmsAction extends Action {
      */
     public function send_post(){
         $to = htmlspecialchars($_POST['phone']);
+        $type = htmlspecialchars($_POST['type']);
         $now = date('YmdHis');
         
         $softVersion = '2014-06-30';
         $appId = '344e70eb358b473493b0edcdcf88a37e';
         $accountSid = '043518e909d6103e10363dcbfb9e40f1';
         $authToken = '9ab4d68c077ef710628959e23491db4b';
-        $templateId = '4124';
 
         $authorization = base64_encode($accountSid.':'.$now);
-        $sig = md5($accountSid.$authToken.$now);
+        $sig = strtoupper(md5($accountSid.$authToken.$now));
         $requestUrl = 'https://api.ucpaas.com/'.$softVersion.'/Accounts/'.$accountSid.'/Messages/templateSMS?sig='.$sig;
 
         $code = rand(100000, 999999);
+        $param = '';
+        $templateId = '';
+        if ($type == 'reg') {
+            $param = '我的快送,'.$code.',2';
+            $templateId = '4124';
+        } else if ($type == 'order') {
+            $param = $code;
+            $templateId = '4596';
+        }
 //        session(array('name'=>'session_id','expire'=>3600));
 //        session('smscode', $code);
-        $templateSMS = json_encode(array('templateSMS' => array('appId' => $appId, 'param'=>'我的快送,'.$code.',3', 'to' => $to, 'templateId' => $templateId)));
+        $templateSMS = json_encode(array('templateSMS' => array('appId' => $appId, 'param'=>$param, 'to' => $to, 'templateId' => $templateId)));
         
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $requestUrl);
@@ -34,15 +43,21 @@ class SmsAction extends Action {
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: api.ucpaas.com', 'Accept:application/json', 'Content-Type:application/json;charset=utf-8', 'Authorization:'.$authorization));
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        $result = json_decode($output, true);
-        echo '<pre>';
-        print_r($result);exit;
-        if ($result['resp']['respCode'] == '000000') {
-            $this->response(array('sms' => $code), 'json');
+        $response = curl_exec($ch);
+        if (curl_getinfo($ch, CURLINFO_HTTP_CODE) == '200') {
+            $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+            curl_close($ch);
+            $responseBody = substr($response, $headerSize);
+            
+            $result = json_decode($responseBody, true);
+            if ($result['resp']['respCode'] == '000000') {
+                $this->response(array('statuscode' => 0, 'msg' => $code), 'json');
+            } else {
+                $this->response(array('statuscode' => 1, 'msg' => '发送短信失败'), 'json');
+            }
         } else {
-            $this->response(array('sms' => '发送短信失败'), 'json');
+            curl_close($ch);
+            $this->response(array('statuscode' => 2, 'msg' => '响应错误'), 'json');
         }
     }
 }
